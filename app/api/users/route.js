be04b865e39b48c52727b1be4ebf3ff728d63a0e
kaziabulhasib <hasibkazi420@gmail.com>
2024-09-12 10:40:21 +0530

    tailwind.congig & global.css file set up

--- app/api/users/route.js
+++
@@ -1,9 +0,0 @@
-export async function GET(request) {
-  const users = [
-    { id: 1, name: "john" },
-    { id: 2, name: "cena" },
-    { id: 3, name: "batista" },
-  ];
-
-  return new Response(JSON.stringify(users));
-}
